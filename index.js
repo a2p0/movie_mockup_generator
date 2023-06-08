f726928d2168d3644c7e842b6bc6a342e7461aac
a2p0 <affranchi2.0@mailoo.org>
2023-06-08 08:19:10 -0400

    STEP fetch request to connect openai api
    Lesson 6

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const apiKey = "#####"
 const url = "https://api.openai.com/v1/completions"
 
+fetch(url, {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`
+  },
+  body: JSON.stringify({
+      model: 'text-davinci-003',
+      prompt: 'What is the capital of Spain?'
+  })
+}).then(response => response.json()).then(data => console.log(data))
+
+
 const setupTextarea = document.getElementById('setup-textarea') 
 const setupInputContainer = document.getElementById('setup-input-container')
 const movieBossText = document.getElementById('movie-boss-text')
